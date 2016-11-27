7802a851da2e9f55fa218764fff29a14963c6a2d
Suissa <jnascimento@gmail.com>
2016-11-27 06:39:20 -0200

    Criado um config para o projeto e para os modulos

--- _config/project.js
+++ _config/project.js
@@ -1,9 +1,15 @@
 const PROJECT_NAME = 'factories'
 const PROJECT_PATH =  __dirname.split(PROJECT_NAME)[0]+PROJECT_NAME
+const QUARKS_PATH = PROJECT_PATH + '/_quarks/'
+const ATOMS_PATH = PROJECT_PATH + '/_atoms/'
 const FACTORIES_PATH = PROJECT_PATH + '/_factories/'
+const MOLECULE_FACTORY = FACTORIES_PATH + 'molecule.mongoose'
 
 module.exports = {
   PROJECT_NAME,
   PROJECT_PATH,
-  FACTORIES_PATH
+  QUARKS_PATH,
+  ATOMS_PATH,
+  FACTORIES_PATH,
+  MOLECULE_FACTORY
 }
--- modules/teste/molecule.js
+++ modules/teste/molecule.js
@@ -1,12 +1,13 @@
-const Factory = require('./../../_factories/molecule.mongoose')
-const QUARKS_PATH = '../../_quarks/'
-const ATOMS_PATH = './../../_atoms/'
+const CONFIG = require('./config')
+const MoleculeFactory = require(CONFIG.MOLECULE_FACTORY)
 
-const name = require(ATOMS_PATH + 'atom-name')
-const email = require(ATOMS_PATH + 'atom-email')
-const description = require(ATOMS_PATH + 'atom-description')
+// Atoms
+const name = require(CONFIG.ATOMS_PATH + 'atom-name')
+const email = require(CONFIG.ATOMS_PATH + 'atom-email')
+const description = require(CONFIG.ATOMS_PATH + 'atom-description')
 const created_at = { type: Date, default: Date.now }
 
+// Molecular Formula
 const Formula = {
   name
 , email
@@ -14,6 +15,6 @@ const Formula = {
 , created_at
 }
 
-const Molecule = Factory(Formula)
+const Molecule = MoleculeFactory(Formula)
 // console.log(Molecule)
 module.exports = Molecule
