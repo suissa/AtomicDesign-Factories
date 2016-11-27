63cf1e1602d161b60e694a0cc5054db09f0636c8
Suissa <jnascimento@gmail.com>
2016-11-27 05:43:40 -0200

    Criando uma CONFIG para o projeto

---
+++ _config/project.js
@@ -0,0 +1,9 @@
+const PROJECT_NAME = 'factories'
+const PROJECT_PATH =  __dirname.split(PROJECT_NAME)[0]+PROJECT_NAME
+const FACTORIES_PATH = PROJECT_PATH + '/_factories/'
+
+module.exports = {
+  PROJECT_NAME,
+  PROJECT_PATH,
+  FACTORIES_PATH
+}
--- modules/teste/organism.js
+++ modules/teste/organism.js
@@ -1,10 +1,12 @@
-const PROJECT_NAME = 'factories'
-const PROJECT_PATH =  __dirname.split(PROJECT_NAME)[0]+PROJECT_NAME
-const FACTORIES_PATH = PROJECT_PATH + '/_factories/'
-console.log('FACTORIES_PATH', FACTORIES_PATH)
+// const PROJECT_NAME = 'factories'
+// const PROJECT_PATH =  __dirname.split(PROJECT_NAME)[0]+PROJECT_NAME
+// const FACTORIES_PATH = PROJECT_PATH + '/_factories/'
+// console.log('FACTORIES_PATH', FACTORIES_PATH)
+
+const CONFIG = require('./../../_config/project')
 
 const name = __filename
-              .split(PROJECT_NAME)[1]
+              .split(CONFIG.PROJECT_NAME)[1]
               .split('modules')[1]
               .split('organism.js')[0]
               .replace(/\//g, '')
@@ -14,6 +16,6 @@ const DNA = {
 	name
   // organelles: ['']
 }
-const Cell = require(FACTORIES_PATH + 'organism')(DNA)
+const Cell = require(CONFIG.FACTORIES_PATH + 'organism')(DNA)
 
 module.exports = Cell
