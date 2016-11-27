7f864ce98825321ef9dbe7fb17ae8290effa69ee
Suissa <jnascimento@gmail.com>
2016-11-27 06:43:40 -0200

    Mudei o nome do modulo de exemplo para User e suas rotas serao /api/users pois coloquei para pluralizar, add s no final, as rotas baseando-se no nome do modulo

--- _config/routes/getRoutes.js
+++ _config/routes/getRoutes.js
@@ -1,5 +1,5 @@
 const MODULES_PATH = './../../modules/'
 
 module.exports = (module, i) => ({ 
-  path: '/api/' + module.toLowerCase(), 
+  path: '/api/' + module.toLowerCase() + 's', 
   module: MODULES_PATH + module })
--- modules/teste/index.js
+++
@@ -1,5 +0,0 @@
-const _router = require('./routerExpress')
-const Routes = require('./routes')(require('./organism'))
-const Router = require('./routesExpress')(Routes, _router)
-
-module.exports = Router
